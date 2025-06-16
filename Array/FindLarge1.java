20a47f826d04adcec74d25357f2d5ba1f56f19d4
Santosh Yadav <santoshgkp107@gmail.com>
2025-06-16 23:27:31 +0530

    Update FindLarge1.java

--- Array/FindLarge1.java
+++ Array/FindLarge1.java
@@ -1,15 +1,11 @@
 package Array;
-
 public class FindLarge1 {static int arr[] = {10, 324, 45, 90, 9808};
-
     // Method to find maximum in arr[]
     static int largest()
     {
         int i;
-
         // Initialize maximum element
         int max = arr[0];
-
         // Traverse array elements from second and
         // compare every element with current max
         for (i = 1; i < arr.length; i++)
